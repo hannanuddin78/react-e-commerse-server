b6ff051272adf9e8eac2848ac00a975eef8611f0
hannanuddin78 <hannanuddin78@gmail.com>
2021-12-06 20:23:33 +0600

    first commit

---
+++ controllers/userCtrl.js
@@ -0,0 +1,160 @@
+const Users = require("../models/userModel");
+const Payments = require("../models/paymentModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const userCtrl = {
+  register: async (req, res) => {
+    try {
+      const { name, email, password } = req.body;
+      const user = await Users.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ msg: "The email address is already exists" });
+      }
+      if (password.length <= 5) {
+        return res
+          .status(400)
+          .json({ msg: "password must be at least 6 characters" });
+      }
+      const passwordHash = await bcrypt.hash(password, 15);
+      const newUser = new Users({ name, email, password: passwordHash });
+      // database save
+      await newUser.save();
+      // Then create jsonwebtoken to authentication
+      const accessToken = createAccessToken({ id: newUser._id });
+      const refreshToken = createRefreshToken({ id: newUser._id });
+
+      res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        path: '/user/refresh_token',
+        maxAge: 7 * 24 * 60 * 60 * 1000 // 7d
+      })
+
+      res.json({ accessToken });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  },
+  login: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const user = await Users.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ msg: "User does not exist." });
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Incorrect password." });
+      }
+
+      const accessToken = createAccessToken({ id: user._id });
+      const refreshToken = createRefreshToken({ id: user._id });
+
+      res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        path: '/user/refresh_token',
+        maxAge: 7 * 24 * 60 * 60 * 1000 // 7d
+      })
+
+      res.json({ accessToken });
+    } catch (error) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  logout: (req, res) => {
+    try {
+      // res.clearCookie("refreshToken", { path: "/user/refresh_token" });
+      res.clearCookie("refreshToken", {
+        httpOnly: true,
+        path: "/user/refresh_token"
+      });
+      return res.json({ msg: "Logged out" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  refreshToken: (req, res) => {
+    try {
+      const ref_token = req.cookies.refreshToken;
+      
+      if (!ref_token) {
+        return res
+          .status(400)
+          .json({ msg: "Please Login or Register for refresh token" });
+      }
+      jwt.verify(ref_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) {
+          return res
+            .status(400)
+            .json({ msg: "Please Login or Register for token verify" });
+        }
+
+        const accessToken = createAccessToken({ id: user.id });
+        
+        res.json({ accessToken });
+      });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  },
+  getUser: async (req, res) => {
+    try {
+      const user = await Users.findById(req.user.id).select("-password");
+      if (!user) {
+        return res.status(400).json({ msg: "User does not exist." });
+      }
+      res.json(user);
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  },
+  addCart: async (req, res) => {
+    try {
+      const user = await Users.findById(req.user.id);
+      if (!user) {
+        return res.status(400).json({ msg: "User does not exist." });
+      }
+      await Users.findByIdAndUpdate(
+        { _id: req.user.id },
+        {
+          cart: req.body.cart,
+        }
+      );
+      return res.status(200).json({ msg: "Added to cart" });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  },
+  history: async (req, res) => {
+    try {
+      const history = await Payments.find({ user_id: req.user.id });
+
+      res.json(history);
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+};
+
+const createAccessToken = (user) => {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "5m" });
+};
+
+const createRefreshToken = (user) => {
+  return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "7d" });
+};
+
+module.exports = userCtrl;
+
+// history: async (req, res) => {
+//   try {
+//     const history = await Payments.find({ user_id: req.user.id });
+
+//     res.json(history);
+//   } catch (err) {
+//     return res.status(500).json({ msg: err.message });
+//   }
+// };
---
+++ server.js
@@ -0,0 +1,53 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
+dotenv.config();
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+app.use(fileUpload({
+    useTempFiles: true
+}))
+
+// Connect to mongodb
+const URI = process.env.MONGODB_URL
+mongoose.connect(URI, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err =>{
+    if(err) throw err;
+    console.log('Connected to MongoDB')
+})
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+const PORT = process.env.PORT || 7000;
+
+app.get('/', (req, res) => {
+    res.json({ msg: `server is running ${PORT}` })
+})
+
+// Routes
+app.use('/user', require('./routes/userRouter'))
+app.use('/api', require('./routes/categoryRouter'))
+app.use('/api', require('./routes/upload'))
+app.use('/api', require('./routes/productRouter'))
+app.use('/api', require('./routes/paymentRouter'))
+
+app.listen(PORT, () =>{
+    console.log('Server is running on port', PORT)
+})
